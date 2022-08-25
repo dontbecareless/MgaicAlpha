53ba4bd1e7e28ae40e7458a2575295cabdb5accc
dontbecareless <tima_guryanov@vk.com>
2022-08-25 10:26:50 +0300

    main block v1 + patch
    Main block
    SUPER technic patch

--- menu.js
+++ menu.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
 	//открываем менюшку статей
-	$('.more_button_st').click(function(event){
-		$('.more_button_st,.submenu_st,.menu_punkt_st').toggleClass('active');
-	});
-	$('.menu_punkt_st').click(function(event){
-		$('.more_button_st,.submenu_st,.menu_punkt_st').toggleClass('active');
+	$('#menu_punkt_st').click(function(event){
+		$('#more_button_st,#menu_st_submenu,#menu_punkt_text_st,#menu_punkt_st').toggleClass('active');
 	});
 	
 	//открываем менюшку колод
-	$('.more_button_de').click(function(event){
-		$('.more_button_de,.submenu_de,.menu_punkt_de').toggleClass('active');
-	});
-	$('.menu_punkt_de').click(function(event){
-		$('.more_button_de,.submenu_de,.menu_punkt_de').toggleClass('active');
+	$('#menu_punkt_de').click(function(event){
+		$('#more_button_de,#menu_de_submenu,#menu_punkt_text_de,#menu_punkt_de').toggleClass('active');
 	});
 	
 	//открываем менюшку аккаунта
-	$('.more_button_ac').click(function(event){
-		$('.more_button_ac,.submenu_ac,.menu_punkt_ac').toggleClass('active');
-	});
-	$('.menu_punkt_ac').click(function(event){
-		$('.more_button_ac,.submenu_ac,.menu_punkt_ac').toggleClass('active');
+	$('#menu_punkt_ac').click(function(event){
+		$('#more_button_ac,#menu_ac_submenu,#menu_punkt_text_ac,#menu_punkt_ac').toggleClass('active');
 	});
 	
 	
@@ -33,25 +24,18 @@ $(document).ready(function(){
 	
 	window.addEventListener('click', e => { 
 		const target = e.target
-		if (!target.closest('.submenu_de,.menu_punkt_de,.more_button_de')){ 
-			//$('.more_button_ac,.submenu_ac,.menu_punkt_ac').removeClass('active');
-			//$('.more_button_st,.submenu_st,menu_punkt_st').removeClass('active');
-			$('.more_button_de,.submenu_de,.menu_punkt_de').removeClass('active');
+		if (!target.closest('#menu_de_submenu,#menu_punkt_de,#more_button_de')){ 
+			$('#more_button_de,#menu_de_submenu,#menu_punkt_de').removeClass('active');
 		}
-		if (!target.closest('.submenu_st,.menu_punkt_st,.more_button_st')){ 
-			//$('.more_button_ac,.submenu_ac,.menu_punkt_ac').removeClass('active');
-			$('.more_button_st,.submenu_st,.menu_punkt_st').removeClass('active');
-			//$('.more_button_de,.submenu_de,.menu_punkt_de').removeClass('active');
+		if (!target.closest('#menu_ac_submenu,#menu_punkt_ac,#more_button_ac')){ 
+			$('#more_button_ac,#menu_ac_submenu,#menu_punkt_ac').removeClass('active');
 		}
-		if (!target.closest('.submenu_ac,.menu_punkt_ac,.more_button_ac')){ 
-			$('.more_button_ac,.submenu_ac,.menu_punkt_ac').removeClass('active');
-			//$('.more_button_st,.submenu_st,menu_punkt_st').removeClass('active');
-			//$('.more_button_de,.submenu_de,.menu_punkt_de').removeClass('active');
+		if (!target.closest('#menu_st_submenu,#menu_punkt_st,#more_button_st')){ 
+			$('#more_button_st,#menu_st_submenu,#menu_punkt_st').removeClass('active');
 		}
 		if (!target.closest('.search_menu,.search_button')){ 
 			$('.search_button,.search_menu').removeClass('active');
-			//$('.more_button_st,.submenu_st,menu_punkt_st').removeClass('active');
-			//$('.more_button_de,.submenu_de,.menu_punkt_de').removeClass('active');
+
 		}
 	})
 });
